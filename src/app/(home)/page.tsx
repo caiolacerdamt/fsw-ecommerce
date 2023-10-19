7970940da4af672f847bffa040f43b021bee4713
caiolacerdamt <caiolacerdamt@gmail.com>
2023-10-19 17:35:29 -0300

    feat: add footer component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Categories from "./components/categories";
 import { prismaClient } from "@/lib/prisma";
 import ProductList from "./components/product-list";
@@ -30,7 +29,7 @@ export default async function Home() {
     },
   });
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex flex-col gap-8 py-8">
       <PromoBanner
         src="/banner-home-01.png"
         alt="Até 55% de desconto esse mês"
